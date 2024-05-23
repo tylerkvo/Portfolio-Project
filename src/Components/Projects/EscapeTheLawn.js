c27e37e136bb885908d7f8a21bdd29cd13edf111
Tyler Vo <tyler.k.vo@gmail.com>
2024-05-23 19:33:23 -0400

    Project Blog Update

---
+++ src/Components/Projects/EscapeTheLawn.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect }from 'react';
+import './project.css'
+import Header from '../Header'
+
+function EscapeTheLawn() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 500); // Threshold for mobile devices
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 500); // Update based on width
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return (
+    <div>
+      <header id="masthead" className="project-header" style={{ backgroundColor: 'white' }}>
+        <div className="s-header__block">
+          <div className="s-header__branding">
+            <p className="site-title">
+              <a href="../" rel="home">tyler vo.</a>
+            </p>
+          </div>
+        </div>
+      </header>
+    <section id="project" class="s-project">
+    <div class="row">
+
+        <div class="column lg-12 intro">
+
+            <h1>Escape the Lawn Video Game</h1>
+
+            
+        </div>
+
+    </div>
+    <div class="row u-add-half-bottom">
+
+                <div class="column lg-6 tab-12" /*style={{ position: 'fixed' }}*/>
+                     {/* <iframe
+                      src="/unitybuild/unityGame.html"
+                      style={{ width: '100%', height: `${window.innerWidth / 4}px`, border: 'none' }}
+                      title="Responsive Video"
+                    ></iframe> */}
+                      {isMobile ? (
+                        <div>
+                          <div className="mobile-message">
+                          Please visit this page on a desktop or laptop computer to play the game.
+                        </div>
+                        </div>
+
+                        ) : (
+                            <iframe
+                            src="/unitybuild/unityGame.html"
+                            style={{ width: '100%', height: `${window.innerWidth / 4}px`, border: 'none' }}
+                            title="Responsive Video"
+                          ></iframe>
+                        )}
+
+                </div>
+
+                <div class="column lg-6 tab-12" /*style={{marginLeft: '50%', overflowY: 'auto'}}*/>
+
+                <p>
+                Exploring game development, a team of 3 friends and I leveraged Unity and C# to create an engaging and interactive video game. Escape the Lawn is a tranquil puzzle adventure through the grand Pavilions of UVA’s historic Lawn, where players must conquer mini UVA-themed quests to execute the grand escape (graduation)! Git was used extensively for version control and team collaboration, allowing for iterative testing and development to improve user experience. Users are immersed through unique NPC interactions, level design, and state management.
+            </p>
+        
+            <h5 class="u-add-bottom">Tools used</h5>
+                    <ul class="skill-bars">
+                        <li>
+                        <div class="progress percent80"><span>80%</span></div>
+                        <strong>Unity</strong>
+                        </li>
+                        <li>
+                        <div class="progress percent20"><span>20%</span></div>
+                        <strong>C Sharp</strong>
+                        </li>
+                        <li>
+                        <div class="progress percent20"><span>20%</span></div>
+                        <strong>Unity Animator</strong>
+                        </li>
+                        <li>
+                        <div class="progress percent40"><span>40%</span></div>
+                        <strong>Unity Tilemap</strong>
+                        </li>
+                        <li>
+                        <div class="progress percent20"><span>20%</span></div>
+                        <strong>Unity Physics</strong>
+                        </li>
+                        <li>
+                        <div class="progress percent10"><span>10%</span></div>
+                        <strong>TextMeshPro</strong>
+                        </li>
+                        
+                        
+                    </ul>
+            <p>
+            Project Blog in progress...
+            </p>
+
+                </div>
+                
+            </div>
+            </section>
+        </div>
+       
+  );
+}
+
+export default EscapeTheLawn;
--- src/Components/Projects/Sabre.js
+++ src/Components/Projects/Sabre.js
@@ -1,9 +1,77 @@
+// import React from 'react';
+// import './project.css'
+
+// function Sabre() {
+//   return (
+//     <div className='project-container'> 
+//       <header id="masthead" className="project-header" style={{ backgroundColor: 'white' }}>
+//         <div className="s-header__block">
+//           <div className="s-header__branding">
+//             <p className="site-title">
+//               <a href="../" rel="home">tyler vo.</a>
+//             </p>
+//           </div>
+//         </div>
+//       </header>
+//       <div className='row'>
+//         <div className="video-column column lg-6 stack-on-900 " style={{ justifyContent: 'center' }}>
+//             <iframe
+//             src="https://marvelapp.com/prototype/9g6jh8i?emb=1&iosapp=false&frameless=false"
+//             style={{ width: '100%', height: '85%', border: 'none' }}
+//             title="Responsive Video"
+//             ></iframe>
+//         </div>
+//         <div className="project-text-column column lg-6 stack-on-900">
+//             <h3>Sabre Mobile App</h3>
+//             <p>
+//             Beginning as a Marvel prototyping project in my Product and Project Management course, I am in the process of implementing Sabre using a React-Native front-end and Django back-end with a close friend. It is a college-focused club and event social media mobile application that allows students to manage their profiles, discover clubs and events, and see what clubs and events their friends are attending!
+//             </p>
+        
+//             <h5 class="u-add-bottom">Tools used</h5>
+                    // <ul class="skill-bars">
+                    //     <li>
+                    //     <div class="progress percent90"><span>80%</span></div>
+                    //     <strong>Marvel</strong>
+                    //     </li>
+                    //     <li>
+                    //     <div class="progress percent40"><span>40%</span></div>
+                    //     <strong>HTML</strong>
+                    //     </li>
+                    //     <li>
+                    //     <div class="progress percent10"><span>10%</span></div>
+                    //     <strong>CSS</strong>
+                    //     </li>
+                    //     <li>
+                    //     <div class="progress percent20"><span>20%</span></div>
+                    //     <strong>Javascript</strong>
+                    //     </li>
+                    //     <li>
+                    //     <div class="progress percent40"><span>40%</span></div>
+                    //     <strong>React-Native</strong>
+                    //     </li>
+                    //     <li>
+                    //     <div class="progress percent10"><span>10%</span></div>
+                    //     <strong>Firebase</strong>
+                    //     </li>
+                        
+                    // </ul>
+//             <p>
+//             Project Blog in progress...
+//             </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sabre;
 import React from 'react';
 import './project.css'
+import Header from '../Header'
 
 function Sabre() {
   return (
-    <div className='project-container'> 
+    <div>
       <header id="masthead" className="project-header" style={{ backgroundColor: 'white' }}>
         <div className="s-header__block">
           <div className="s-header__branding">
@@ -13,22 +81,30 @@ function Sabre() {
           </div>
         </div>
       </header>
-      <div className='row'>
-        <div className="video-column" style={{ justifyContent: 'center' }}>
-            <iframe
-            src="https://marvelapp.com/prototype/9g6jh8i?emb=1&iosapp=false&frameless=false"
-            style={{ width: '100%', height: '85%', border: 'none' }}
-            title="Responsive Video"
-            ></iframe>
+    <section id="project" class="s-project">
+    <div class="row">
+        <div class="column lg-12 intro">
+            <h1>Sabre Mobile App Prototyping</h1>
         </div>
-        <div className="project-text-column">
-            <h3>Sabre Mobile App</h3>
-            <p>
-            Beginning as a Marvel prototyping project in my Product and Project Management course, I am in the process of implementing Sabre using a React-Native front-end and Django back-end with a close friend. It is a college-focused club and event social media mobile application that allows students to manage their profiles, discover clubs and events, and see what clubs and events their friends are attending!
-            </p>
+    </div>
+    <div class="row u-add-half-bottom">
+
+                <div class="column lg-6 tab-12" /*style={{ position: 'fixed' }}*/>
+                    <iframe
+                        src="https://marvelapp.com/prototype/9g6jh8i?emb=1&iosapp=false&frameless=false"
+                        style={{ width: '100%', height: '60%', border: 'none' }}
+                        title="Responsive Video"
+                        ></iframe>
+                </div> 
+
+                <div class="column lg-6 tab-12" /*style={{marginLeft: '50%', overflowY: 'auto'}}*/>
+
+                <p>
+                  Beginning as a Marvel prototyping project in my Product and Project Management course, I am in the process of implementing Sabre using a React-Native front-end and Django back-end with a close friend. It is a college-focused club and event social media mobile application that allows students to manage their profiles, discover clubs and events, and see what clubs and events their friends are attending!
+                </p>
         
-            <h5 class="u-add-bottom">Tools used</h5>
-                    <ul class="skill-bars">
+                  <h5 class="u-add-bottom">Tools used</h5>
+                  <ul class="skill-bars">
                         <li>
                         <div class="progress percent90"><span>80%</span></div>
                         <strong>Marvel</strong>
@@ -55,12 +131,16 @@ function Sabre() {
                         </li>
                         
                     </ul>
-            <p>
-            Project Blog in progress...
-            </p>
+                  <p>
+                  Project Blog in progress...
+                  </p>
+
+                </div>
+                
+            </div>
+            </section>
         </div>
-      </div>
-    </div>
+       
   );
 }
 
